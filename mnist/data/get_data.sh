c325fae0d0436f8167235469ca0ac248371781a7
Vikas Reddy <vikas.challaram@gmail.com>
2016-12-26 16:47:10 +0530

    added get_data.sh for mnist

--- mnist/data/get_data.sh
+++ mnist/data/get_data.sh
@@ -1,3 +1,9 @@
 echo "Downloading..."
 
-wget --no-check-certificate http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+for fname in train-images-idx3-ubyte train-labels-idx1-ubyte t10k-images-idx3-ubyte t10k-labels-idx1-ubyte
+do
+    if [ ! -e $fname ]; then
+        wget --no-check-certificate http://yann.lecun.com/exdb/mnist/${fname}.gz
+        gunzip ${fname}.gz
+    fi
+done
